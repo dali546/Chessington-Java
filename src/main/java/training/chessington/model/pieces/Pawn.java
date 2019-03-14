6eb41c0e89a946251ba6d551192a5734956efe16
dali546 <35352237+dali546@users.noreply.github.com>
2019-03-14 11:45:44 +0000

    Test 3
    Passed not allowed to move Pawn if blocked infront

--- src/main/java/training/chessington/model/pieces/Pawn.java
+++ src/main/java/training/chessington/model/pieces/Pawn.java
@@ -1,12 +1,10 @@
 package training.chessington.model.pieces;
 
-import training.chessington.model.Board;
-import training.chessington.model.Coordinates;
-import training.chessington.model.Move;
-import training.chessington.model.PlayerColour;
+import training.chessington.model.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Pawn extends AbstractPiece {
@@ -16,11 +14,22 @@ public class Pawn extends AbstractPiece {
 
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
+        return PlayerColour.WHITE == colour ? WhiteMoves(from, board) : BlackMoves(from, board);
+    }
+
+    private List<Move> BlackMoves(Coordinates from, Board board) {
+        if (board.get(new Coordinates(from.getRow() + 1, from.getCol())) != null) return new ArrayList<>();
         return new ArrayList<Move>() {{
-            if (colour == PlayerColour.BLACK) add(new Move(from, from.plus(1, 0)));
-            if (colour == PlayerColour.WHITE) add(new Move(from, from.plus(-1, 0)));
-            if (colour == PlayerColour.WHITE && from.getRow()==6) add(new Move(from,from.plus(-2,0)));
-            if (colour == PlayerColour.BLACK && from.getRow()==1) add(new Move(from,from.plus(2,0)));
+            add(new Move(from, from.plus(1, 0)));
+            if (from.getRow() == 1) add(new Move(from, from.plus(2, 0)));
+        }};
+    }
+
+    private List<Move> WhiteMoves(Coordinates from, Board board) {
+        if (board.get(new Coordinates(from.getRow() - 1, from.getCol())) != null) return new ArrayList<>();
+        return new ArrayList<Move>() {{
+            add(new Move(from, from.plus(-1, 0)));
+            if (from.getRow() == 6) add(new Move(from, from.plus(-2, 0)));
         }};
     }
 }
